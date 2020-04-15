c75cc0703e12392dc5461f88b13c93ebb3968a46
Nhut Le <leminhnhut08t1@gmail.com>
2020-04-15 09:43:19 +0700

    initial login with username and password

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
