83e2e42cf2ed114d488e6d6875a961108673c1dc
Nhut Le <leminhnhut08t1@gmail.com>
2020-04-15 15:53:42 +0700

    CsvData + Keyword models

---
+++ app/Models/CsvData.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CsvData extends Model
+{
+    protected $table = 'csv_data';
+    protected $fillable = ['csv_filename', 'csv_data'];
+}
