ac9e44eebad69d3153b7abcf416bcde184c890ce
Nhut Le <leminhnhut08t1@gmail.com>
2020-04-16 11:55:51 +0700

    add Statistic model + its migration

---
+++ database/migrations/2020_04_15_154809_create_statistics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatisticsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statistics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('keyword');
+            $table->unsignedInteger('total_adwords');
+            $table->unsignedInteger('total_links');
+            $table->unsignedBigInteger('total_search_results');
+            $table->text('html_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statistics');
+    }
+}
