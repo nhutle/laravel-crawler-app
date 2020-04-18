46403e261d038dcf8f6fcdab1a57a1afc620f084
Nhut Le <leminhnhut08t1@gmail.com>
2020-04-18 12:08:17 +0700

    logout should be post

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', 'Api\AuthController@login');
-Route::get('/logout', 'Api\AuthController@logout');
+Route::post('login', 'Api\AuthController@login');
+Route::post('logout', 'Api\AuthController@logout');
