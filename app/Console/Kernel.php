60938f9fd45b6111f7bd9cdbd734d629b3a64baf
Nhut Le <leminhnhut08t1@gmail.com>
2020-04-24 10:51:56 +0700

    soft delete tasks weekly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        "App\Console\Commands\Crawler"
+        "App\Console\Commands\Crawler",
+        "App\Console\Commands\SoftDeleteTasks"
     ];
 
     /**
@@ -25,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('crawler:run')->everyMinute()->withoutOverlapping(90);
+        $schedule->command('softDeleteTasks:run')->weekly()->withoutOverlapping(30);
     }
 
     /**
